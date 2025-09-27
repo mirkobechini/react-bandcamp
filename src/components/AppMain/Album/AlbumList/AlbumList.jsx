6eae55479581bf227b43116acc793359206ba08f
mirkobechini <mirkobechini@gmail.com>
2025-09-27 16:07:30 +0200

    responsive: made AlbumList.jsx responsive

--- src/components/AppMain/Album/AlbumList/AlbumList.jsx
+++ src/components/AppMain/Album/AlbumList/AlbumList.jsx
@@ -7,7 +7,7 @@ export default function AlbumList({album_list}){
         <>
         {album_list.map(album =>
 
-        <div className="col-3" key={album.id}>
+        <div className="col-6 col-lg-3" key={album.id}>
             <AlbumCard
                 album = {album}
             />
