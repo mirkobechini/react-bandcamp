8bf4bac74f4722d07cf0d0ea50e9dc648b237a0a
mirkobechini <mirkobechini@gmail.com>
2025-09-27 15:52:36 +0200

    fix: solved problem issue form field id

--- src/components/AppHeader/AppSearchBar.jsx
+++ src/components/AppHeader/AppSearchBar.jsx
@@ -8,7 +8,7 @@ export default function AppSearchBar(props) {
                         <span className={`input-group-text app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-end-0 rounded-start-pill"} `} >
                             <i className="bi bi-search"></i>
                         </span>
-                        <input className={`form-control me-2 app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-start-0 rounded-end-pill"} `} type="search" placeholder={`${(props.father == "navbar") ? "Search and discover music" : "Add a genre, location, or tag"} `} aria-label="Search" />
+                        <input id="searchBar" className={`form-control me-2 app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-start-0 rounded-end-pill"} `} type="search" placeholder={`${(props.father == "navbar") ? "Search and discover music" : "Add a genre, location, or tag"} `} aria-label="Search" />
                     </div>
                 </form>
             </div>
