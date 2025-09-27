ab635ccce3677bc5d28b4cf56284c9732119c4e4
mirkobechini <mirkobechini@gmail.com>
2025-09-27 12:14:45 +0200

    add: create components for main

--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -1,6 +1,15 @@
+//import component
+import AlbumList from "./Album/AlbumList/AlbumList"
+import AlbumActive from "./Album/AlbumActive/AlbumActive"
+
+
 export default function AppMain(){
     return(
         <>
+        <div id="mainContainer" className="app-bg-primary">
+        <AlbumList/>
+        <AlbumActive/>
+        </div>
         </>
     )
 }
