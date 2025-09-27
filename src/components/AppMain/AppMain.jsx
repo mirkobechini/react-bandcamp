fbbb7aaed0171988904b072db5fdcdda0e8bf39e
mirkobechini <mirkobechini@gmail.com>
2025-09-27 17:31:04 +0200

    responsive: made AppMain responsive

--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -8,13 +8,13 @@ export default function AppMain({ album_list }) {
         <>
             <div id="app-mainContainer" className="app-bg-primary p-4">
                 <div className="row ">
-                    <div className="col-8 pe-3">
+                    <div className="col-md-8 pe-3">
                         <div className="row">
 
                             <AlbumList album_list={album_list} />
                         </div>
                     </div>
-                    <div className="col-4 border-start ps-3">
+                    <div className="col-4 border-start ps-3 d-none d-md-block">
                         <AlbumActive album_list={album_list}/>
                     </div>
                 </div>
