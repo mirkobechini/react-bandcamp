57d5112e63b774bc29958dc6cc1b66561aba2fa1
mirkobechini <mirkobechini@gmail.com>
2025-09-27 15:51:41 +0200

    style: adjust header searchbar

--- src/components/AppHeader/AppSearchBar.jsx
+++ src/components/AppHeader/AppSearchBar.jsx
@@ -3,7 +3,7 @@ export default function AppSearchBar(props) {
     return (
         <>
             <div className="container-fluid" >
-                <form className="d-flex col-3" role="search">
+                <form className="d-flex col-4" role="search">
                     <div className="input-group py-2 app-text-subtitle">
                         <span className={`input-group-text app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-end-0 rounded-start-pill"} `} >
                             <i className="bi bi-search"></i>
