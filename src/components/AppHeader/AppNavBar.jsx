2ff21415a028f2a5b335269fa7977f8ab9c6ffbb
mirkobechini <mirkobechini@gmail.com>
2025-09-27 11:36:40 +0200

    style: completed searchbar css

--- src/components/AppHeader/AppNavBar.jsx
+++ src/components/AppHeader/AppNavBar.jsx
@@ -7,17 +7,17 @@ import AppSearchBar from "./AppSearchBar.jsx"
 export default function AppNavBar({nav_menu}) {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <nav className="navbar navbar-expand-lg app-bg-primary">
                 <div className="container-fluid">
                     <img className="navbar-brand" src={nav_logo} width="150" />
                     
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <AppSearchBar
                         father = "navbar"
                     />
-                    <div className="collapse navbar-collapse col-1" id="navbarNavDropdown">
+                    <div className="collapse navbar-collapse col-1" id="navbarNav">
                         <ul className="navbar-nav">
                             {
                                 nav_menu.map(item =>
--- src/components/AppHeader/AppSearchBar.jsx
+++ src/components/AppHeader/AppSearchBar.jsx
@@ -4,11 +4,11 @@ export default function AppSearchBar(props) {
         <>
             <div className="container-fluid" >
                 <form className="d-flex col-3" role="search">
-                    <div className="input-group py-2 ">
-                        <span className={`input-group-text app-bg-tertiary ${(props.father == "navbar") ? "border-0" : "border-end-0 rounded-start-pill"} `} >
+                    <div className="input-group py-2 app-text-subtitle">
+                        <span className={`input-group-text app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-end-0 rounded-start-pill"} `} >
                             <i className="bi bi-search"></i>
                         </span>
-                        <input className={`form-control me-2 app-bg-tertiary ${(props.father == "navbar") ? "border-0" : "border-start-0 rounded-end-pill"} `} type="search" placeholder={`${(props.father == "navbar") ? "Search and discover music" : "Add a genre, location, or tag"} `} aria-label="Search" />
+                        <input className={`form-control me-2 app-searchbar-item ${(props.father == "navbar") ? "border-0" : "border-start-0 rounded-end-pill"} `} type="search" placeholder={`${(props.father == "navbar") ? "Search and discover music" : "Add a genre, location, or tag"} `} aria-label="Search" />
                     </div>
                 </form>
             </div>
